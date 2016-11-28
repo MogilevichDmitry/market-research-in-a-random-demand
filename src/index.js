ac543f2167f3774e7b8ce80672dc191c789ac3be
MogilevichDmitry <dmsmain@gmail.com>
2016-11-28 10:51:48 +0300

    Add express and jade

--- src/index.js
+++ src/index.js
@@ -1,62 +1,22 @@
-const gaussian = (mean, stdev) => {
-  let y2;
-  let use_last = false;
-  return function() {
-    let y1;
+import express from 'express';
+import { simulateRandomProcess } from './api';
 
-    if(use_last) {
-      y1 = y2;
-      use_last = false;
-    } else {
-      let x1, x2, w;
+const app = express();
 
-      do {
-        x1 = 2.0 * Math.random() - 1.0;
-        x2 = 2.0 * Math.random() - 1.0;
-        w = x1 * x1 + x2 * x2;
-      } while( w >= 1.0);
+app.set('view engine', 'jade');
 
-      w = Math.sqrt((-2.0 * Math.log(w)) / w);
-      y1 = x1 * w;
-      y2 = x2 * w;
-      use_last = true;
-    }
+app.get('/process', (req, res) => {
+  const randomProcess = simulateRandomProcess(1/4, 600);
+  res.render('process.jade', {
+    lambda: '1/4',
+    T_n: '600',
+    count: randomProcess.count,
+    sigma: randomProcess.sigma,
+    array: randomProcess.t_array
+   })
+})
 
-    var retval = mean + stdev * y1;
-    if(retval > 0) {
-      return retval;
-    }
-    return -retval;
-  }
-}
 
-const getGaussian = gaussian(0, 1);
-
-const getRandom = () => {
-  let randomNumber = getGaussian();
-  return randomNumber >= 1 ? getRandom() : randomNumber;
-}
-
-const simulateRandomProcess = (lambda, T_n ) => {
-  let n = 0;
-  let t_n;
-  let t = [0];
-  let r, tau;
-  let flag = true;
-
-  while(flag) {
-    n++;
-    r = getRandom();
-    tau = -1.0/lambda * Math.log(r);
-    t_n = t[n - 1] + tau;
-    if (t_n > T_n){
-      flag = false;
-    } else {
-      t.push(t_n);
-    }
-  }
-  return { t_array: t, count: n - 1, sigma: T_n / n - 1 };
-}
-
-const randomProcess = simulateRandomProcess(1/4, 1000);
-console.log(randomProcess);
+app.listen(3000, () => {
+  console.log('Server is available on http://localhost:3000');
+})
