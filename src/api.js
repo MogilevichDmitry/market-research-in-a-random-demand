ccded08ac3cbade8ff2e0e3c2d213eeaf9b64ec1
Дмитрий Могилевич <DmitryMogilevich@MacBook-Pro-Dmitrij.local>
2016-11-30 14:37:48 +0300

    Rework equilibrium market functionality, Add deficit market functionality

--- src/api.js
+++ src/api.js
@@ -21,11 +21,45 @@ export const simulateRandomProcess = (lambda, T_n ) => {
   return { t_array: t, count: n - 1, sigma: T_n / n - 1 };
 }
 
-const simulateTradeDeficitMarket = () => {
-
+export const simulatePseudoProcess = (T) => {
+  let pseudoProcess = [];
+  for(let i = 0; i < T; i++) {
+    pseudoProcess.push((Math.random() - 0.5));
+  }
+  return pseudoProcess;
 }
-const simulateOverstockMarket = () => {
 
+export const simulateTradeDeficitMarket = (
+  R = 50,
+  Q_m = 4,
+  a = 0.4,
+  T = 300,
+  P_0 = 7,
+  P_1 = 3,
+  P_2 = 0.1,
+  Q_0 = 0,
+  tau = 10,
+  sigma = 0.01
+) => {
+  const P_min = P_1 + P_2;
+  const P_max = Q_m / a;
+  const P_t = [P_0];
+  const Q_t = [0];
+  const P1_t = [7];
+  let tempP_t, tempQ_t, tempP1_t;
+  const randomProcess = simulatePseudoProcess(T);
+
+  for(let t = 1; t < T; t++) {
+    tempQ_t = (R * (Q_m + randomProcess[t] - a * P_t[t - 1])) / (a + R);
+    tempP_t = P_t[t - 1] + tempQ_t / R;
+    Q_t.push(tempQ_t);
+    P_t.push(tempP_t);
+  }
+
+  return { Q_t: Q_t, P_t: P_t};
+}
+
+export const simulateOverstockMarket = () => {
 }
 
 export const simulateEquilibriumMarket = (
@@ -44,21 +78,16 @@ export const simulateEquilibriumMarket = (
   const P_max = Q_m / a;
   const P_t = [P_0];
   const Q_t = [0];
-  let tempP_t, tempQ_t;
-  const randomProcess = simulateRandomProcess(1/10, 300);
-  const processAcross = Math.ceil(T / randomProcess.count);
-  let processCount = 0;
+  const P1_t = [7];
+  let tempP_t, tempQ_t, tempP1_t;
+  const randomProcess = simulatePseudoProcess(T);
 
-  for(let t = 1; t <= T; t++) {
-    if( t % processAcross === 0) {
-      processCount++;
-    }
-    tempQ_t = (R * (Q_m + randomProcess.t_array[processCount] - a * P_t[t - 1] ) + a * (Q_m + randomProcess.t_array[processCount] - a * P_1)) / (2 * a + R);
-    tempP_t = (Q_m + randomProcess.t_array[processCount] + R * P_t[t - 1]) / (2 * a + R);
+  for(let t = 1; t < T; t++) {
+    tempQ_t = (R * (Q_m + randomProcess[t] - a * P_t[t - 1] ) + a * (Q_m + randomProcess[t] - a * P_1)) / (2 * a + R);
+    tempP_t = (Q_m + randomProcess[t] - tempQ_t) / a;
     Q_t.push(tempQ_t);
     P_t.push(tempP_t);
-    console.log(processCount);
   }
 
-  return { Q_t: Q_t, P_t: P_t };
+  return { Q_t: Q_t, P_t: P_t};
 }
--- src/index.js
+++ src/index.js
@@ -1,28 +1,47 @@
 import express from 'express';
-import { simulateRandomProcess, simulateEquilibriumMarket } from './api';
+import {
+  simulateRandomProcess,
+  simulateEquilibriumMarket,
+  simulateTradeDeficitMarket,
+  simulateOverstockMarket
+ } from './api';
 
 const app = express();
 
 app.set('view engine', 'jade');
 
-app.get('/process', (req, res) => {
-  const randomProcess = simulateRandomProcess(1/2, 300);
-  res.render('process.jade', {
-    lambda: '1/2',
-    T_n: '300',
-    count: randomProcess.count,
-    sigma: randomProcess.sigma,
-    array: randomProcess.t_array
-   })
-})
+// app.get('/process', (req, res) => {
+//   const randomProcess = simulateRandomProcess(1/2, 300);
+//   res.render('process.jade', {
+//     lambda: '1/2',
+//     T_n: '300',
+//     count: randomProcess.count,
+//     sigma: randomProcess.sigma,
+//     array: randomProcess.t_array
+//    })
+// })
 
 app.get('/equilibrium', (req, res) => {
   const equilibriumMarket = simulateEquilibriumMarket();
+  equilibriumMarket.P_t = equilibriumMarket.P_t.map(number => String(number).replace('.', ','));
+  equilibriumMarket.Q_t = equilibriumMarket.Q_t.map(number => String(number).replace('.', ','))
   res.render('result', {
     Q_t: equilibriumMarket.Q_t,
     q_length: equilibriumMarket.Q_t.length,
     P_t: equilibriumMarket.P_t,
-    p_length: equilibriumMarket.P_t.length,
+    p_length: equilibriumMarket.P_t.length
+  })
+})
+
+app.get('/deficit', (req, res) => {
+  const deficitMarket = simulateTradeDeficitMarket();
+  deficitMarket.P_t = deficitMarket.P_t.map(number => String(number).replace('.', ','));
+  deficitMarket.Q_t = deficitMarket.Q_t.map(number => String(number).replace('.', ','))
+  res.render('result', {
+    Q_t: deficitMarket.Q_t,
+    q_length: deficitMarket.Q_t.length,
+    P_t: deficitMarket.P_t,
+    p_length: deficitMarket.P_t.length
   })
 })
 
